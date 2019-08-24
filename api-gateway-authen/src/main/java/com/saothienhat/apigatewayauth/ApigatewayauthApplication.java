2e7ceab78d84c3266c6b6048c1a151574cf00b9e
saothienhat <saothienhat@gmail.com>
2019-08-24 21:41:10 +0700

    Add CORS option for API Gateway
    Add CORS option for API Gateway

--- api-gateway-authen/src/main/java/com/saothienhat/apigatewayauth/ApigatewayauthApplication.java
+++ api-gateway-authen/src/main/java/com/saothienhat/apigatewayauth/ApigatewayauthApplication.java
@@ -4,8 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -17,6 +23,19 @@ public class ApigatewayauthApplication {
 		LOGGER.info("API Gateway Auth running...");
 		SpringApplication.run(ApigatewayauthApplication.class, args);
 	}
+
+	@Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
 	
 
 }
